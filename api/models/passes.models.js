a287b0256899d101c4ffa372e625ce3e010f7a69
Nick Brakis <nibrakis@gmail.com>
2022-01-02 15:50:43 +0200

    chargesBy commit

--- api/models/passes.models.js
+++ api/models/passes.models.js
@@ -85,4 +85,51 @@ Passes.passesAnalysis = (op1_id, op2_id, date_from, date_to, result) => {
   });
 };
 
+Passes.chargesBy = (op_id, date_from, date_to, result) => {
+  const dateForm = 'YYYY-MM-DD HH:mm:ss';
+  const reqTimestamp = moment().format(dateForm);
+  sql.query(`SELECT COUNT(passes_pass_id) as no_of_passes, SUM(passes_charge) as total_charge, vehicles_tag_abbr
+            FROM passes LEFT JOIN vehicles
+            ON passes.passes_vehicle_id = vehicles.vehicles_vehicle_id
+            WHERE LEFT(passes_station_id, 2) = '${op_id}' AND vehicles_tag_abbr != '${op_id}' AND passes_timestamp > '${date_from}' AND passes_timestamp < '${date_to}'
+            GROUP BY vehicles_tag_abbr;`, function (err, rows)  {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+    var string = JSON.stringify(rows)
+    var json_res = JSON.parse(string)
+
+    list = new Array();
+    for (var i = 0; i < rows.length; i++){
+      list.push({
+        VisitingOperator : json_res[i].vehicles_tag_abbr,
+        NumberOfPasses : json_res[i].no_of_passes,
+        PassesCost : json_res[i].total_charge,
+      });
+    }
+    var string_list = JSON.stringify(list)
+    var ppoList = JSON.parse(string_list) 
+    const sendRes = {
+      op_ID : op_id,
+      requestedTimestamp : reqTimestamp,
+      PeriodFrom : moment(date_from).format(dateForm),
+      PeriodTo : moment(date_to).format(dateForm),
+      PPOList : ppoList
+    };
+    
+
+    if (rows.length) {
+      console.log("found passes: \n", sendRes);
+      result(null, sendRes);
+      return;
+    }
+
+    // not found Passes with the id
+    result({ kind: "not_found" }, null);
+  });
+};
+
+
 module.exports = Passes;
--- api/routes/passes.routes.js
+++ api/routes/passes.routes.js
@@ -1,6 +1,7 @@
 module.exports = app => {
     const passesByStation = require("../controllers/passesByStation.controllers.js");
     const passesAnalysis = require("../controllers/passesAnalysis.controllers.js");
+    const chargesBy = require("../controllers/chargesBy.controllers.js")
   
     var router = require("express").Router();
     
@@ -9,6 +10,10 @@ module.exports = app => {
   
     // Retrieve passesByStationID
     router.get("/", passesByStation.getAll);
+
+    // Retrieve ChargesBy
+    router.get("/ChargesBy/:op_ID/:date_from/:date_to", chargesBy.getChargesBy);
+
     
     app.use('/api/passes', router);
   };
