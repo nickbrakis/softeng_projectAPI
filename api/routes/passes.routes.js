0a4f62e2a8d02f5a82b381023c2b14c7b521b022
Nick Brakis <nibrakis@gmail.com>
2022-01-02 14:29:20 +0200

    commit_test

--- api/routes/passes.routes.js
+++ api/routes/passes.routes.js
@@ -3,27 +3,12 @@ module.exports = app => {
     const passesAnalysis = require("../controllers/passesAnalysis.controllers.js");
   
     var router = require("express").Router();
-  
-    // // Create a new Tutorial
-    // router.post("/", tutorials.create);
-  
+    
     // Retrieve PassesAnalysis
     router.get("/PassesAnalysis/:op1_ID/:op2_ID/:date_from/:date_to", passesAnalysis.getPasses);
   
     // Retrieve passesByStationID
     router.get("/", passesByStation.getAll);
-  
-    // // Retrieve a single Tutorial with id
-    // router.get("/:id", tutorials.findOne);
-  
-    // // Update a Tutorial with id
-    // router.put("/:id", tutorials.update);
-  
-    // // Delete a Tutorial with id
-    // router.delete("/:id", tutorials.delete);
-  
-    // // Delete all Tutorials
-    // router.delete("/", tutorials.deleteAll);
-  
+    
     app.use('/api/passes', router);
   };
